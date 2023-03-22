e54d2b13639f1840120b5b30e38f2beaf2833862
IsabelliSS <isabellisantiago.souza@gmail.com>
2023-03-22 08:28:45 -0300

    variaveis e tipos

---
+++ aula02/variaveis.js
@@ -0,0 +1,28 @@
+// let num1 = 2
+// const num2 = 3
+// var msg = "olá amigos! Esse é o número:"
+
+// console.log(msg + num1)
+// console.log(num1 + num2)
+// console.log(typeof num1)
+// console.log(typeof msg)
+
+
+// -----------------------------------------------------------------------------
+
+// JSON = JavaScript Object Natation
+// let usuario ={
+//     nome: "Isa",
+//     idade: 17,
+//     signo: "libra",
+//     calvo: true
+// }
+// console.log(usuario)
+// console.log("nome:", usuario.nome, '\n', "Idade:", usuario.idade)
+// console.log(`nome do usuario ${usuario.nome}`)
+
+// -------------------------------------------------------------
+const nomes = ["Isa", "Juninho"]
+console.log(nomes)
+console.log(typeof nomes)
+console.log(nomes[0])
